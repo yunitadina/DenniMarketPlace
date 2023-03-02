7c2380005bd853ba2bfe62dbb490fc40701d56ba
yunitadina <126739502+yunitadina@users.noreply.github.com>
2023-03-02 09:44:57 +0700

    Create jsonnft.js

---
+++ jsonnft.js
@@ -0,0 +1,17 @@
+{
+    "name": "Denniwae",
+    "symbol": "DNNW",
+    "description": "Denniwae Marketplace by Mirror World offers multi-chain social authentication, MPC wallet solutions, and in-app marketplace solutions, all with 0 upfront cost.",
+    "image": "https://metadata-assets.mirrorworld.fun/sdk/images/mw_logo.png",
+    "seller_fee_basis_points": 425,
+    "collection": {
+        "name": "Denniwae Marketplace",
+        "family": "Denniwae Marketplace"
+    },
+    "attributes": [
+        {
+            "trait_type": "Rarity",
+            "value": "Common"
+        }
+    ]
+}
